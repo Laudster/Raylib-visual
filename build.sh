cdfc001040de4acba6dd565d9068566215a16c15
Amund <amundkampen@gmail.com>
2025-07-06 22:45:03 +0200

    Small fixes

--- build.sh
+++ build.sh
@@ -1,17 +1,38 @@
-#!/bin/bash
+RAYLIB_PATH="/home/dependencies/raylib" # Adjust if raylib is somewhere else
 
-# Set the directory passed as the argument
-DIR="$1"
-OUTPUT_DIR="$DIR/web-build"
-SOURCE_FILE="$DIR/project_file.c"
+echo "Starting Emscripten compilation..."
+echo "Source: ${SOURCE_FILE}"
+echo "Output: ${OUTPUT_DIR}/project.html"
 
-# Run the emcc command
-emcc -o "$OUTPUT_DIR/project.html" "$SOURCE_FILE" -Wall -std=c99 \
-    -D_DEFAULT_SOURCE -Wno-missing-braces -Wunused-result -Os -I. \
-    -I C:/raylib/raylib/src -I C:/raylib/raylib/src/external -L. \
-    -L C:/raylib/raylib/src -s USE_GLFW=3 -s ASYNCIFY -s TOTAL_MEMORY=67108864 \
-    -s FORCE_FILESYSTEM=1 --preload-file sounds \
-    --shell-file C:/raylib/raylib/src/shell.html \
-    C:/raylib/raylib/src/web/libraylib.a -DPLATFORM_WEB \
-    -s "EXPORTED_FUNCTIONS=[_free,_malloc,_main]" \
-    -s EXPORTED_RUNTIME_METHODS=ccall
+# Create the output directory if it doesn't exist
+mkdir -p "${OUTPUT_DIR}"
+
+emcc -o "${OUTPUT_DIR}/project.html" \
+  "${SOURCE_FILE}" \
+  -Wall \
+  -std=c99 \
+  -D_DEFAULT_SOURCE \
+  -Wno-missing-braces \
+  -Wunused-result \
+  -Os \
+  -I. \
+  -I "${RAYLIB_PATH}/src" \
+  -I "${RAYLIB_PATH}/src/external" \
+  -L. \
+  -L "${RAYLIB_PATH}/src" \
+  -s USE_GLFW=3 \
+  -s ASYNCIFY \
+  -s TOTAL_MEMORY=67108864 \
+  -s FORCE_FILESYSTEM=1 \
+  --shell-file "${RAYLIB_PATH}/src/shell.html" \
+  "${RAYLIB_PATH}/src/web/libraylib.web.a" \
+  -DPLATFORM_WEB \
+  -s "EXPORTED_FUNCTIONS=[_free,_malloc,_main]" \
+  -s EXPORTED_RUNTIME_METHODS=ccall
+
+if [ $? -eq 0 ]; then
+    echo "Emscripten compilation successful!"
+else
+    echo "Emscripten compilation failed!"
+    exit 1
+fi
